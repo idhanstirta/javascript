e96cd0d96f277452dab545601da9cae25566cf2d
Hans Tirta <hanstirta235@gmail.com>
2025-10-29 09:32:40 +0700

    Add example of object literal operations
    Demonstrate object literal usage with properties, access, modification, and deletion.

---
+++ object-literal.js
@@ -0,0 +1,30 @@
+// object literal
+let person2 = {}
+
+let person = {
+  firstName: 'Hans', // properties
+  lastName: 'Tirta', // properties
+  'building no': 2345 // properties
+};
+// mengakses properties dengan dot notation(.)
+person.firstName
+// mengakses properties dengan array like notation([])
+person['lastName']
+person['building no'];
+// merubah value property
+person.firstName = 'Dumbo';
+person 
+// menambahkan property baru ke sebuah object
+person.age = 25;
+person 
+// menghapus property pada sebuah object
+delete person.age;
+person 
+// mengecek property ada atau tidak
+'firstName' in person 
+'ssn' in person 
+
+
+
+
+
