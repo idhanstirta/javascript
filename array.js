937f496543010f9c7b40258aa07ee780fa7ad391
Hans Tirta <hanstirta235@gmail.com>
2025-10-29 21:16:13 +0700

    Add array manipulation examples in array.js
    This file demonstrates array creation, manipulation, and basic operations in JavaScript.

---
+++ array.js
@@ -0,0 +1,29 @@
+// membuat array dengan array constructor
+let score = new Array();
+score
+let scores = new Array(1,2,3,4,5,6,7,8,9);
+scores
+// membuat array dengan array literal
+let colors = ['red', 'green', 'blue', 'yellow']
+colors
+// mengakses array element
+colors[0]
+// mendapatkan size array
+colors.length
+// operasi dasar di array
+// 1. push() : menambahkan element ke array
+colors.push('black');
+colors
+// 2. unshift() : menambahkan element ke urutan pertama array
+colors.unshift('silver');
+colors
+// 3. pop() : menghapus element dari akhir array
+colors.pop();
+colors
+// 4. shift() : menghapus element dari pertama array
+colors.shift();
+colors
+// 5. indexOf() : menemukan indeks element dari array
+colors.indexOf('blue');
+// 6. Array.isArray() : mengecek jika sebuah nilai adalah sebuah array atau bukan
+Array.isArray(colors);
