73b57636e687151b78a1750fcc0435ebfa40f553
Hans Tirta <hanstirta235@gmail.com>
2025-10-30 09:47:27 +0700

    Implement if and nested if statement examples
    Add example of if and nested if statements

---
+++ if-statement.js
@@ -0,0 +1,13 @@
+// if statement
+let age = 18;
+if (age >= 18){
+  'you can drive';
+}
+// if didalam if atau nested if statement
+let ages = 21;
+let cs = 'ccs';
+if (cs == 'ccs'){
+  if (ages >= 21) {
+    'you can swim';
+  }
+}
