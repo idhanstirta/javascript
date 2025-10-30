fc9492dd84511b00bcf6cd8cae89410b6f74f2f9
Hans Tirta <hanstirta235@gmail.com>
2025-10-30 19:07:25 +0700

    Add while loop examples to while-loop.js

---
+++ while-loop.js
@@ -0,0 +1,12 @@
+// while loop
+let count = 1;
+while (count < 10) {
+    console.log(count);
+    count +=2;
+}
+let mount = ['q','w','e','r','t','y'];
+let i = 0;
+while(i<mount.length){
+  console.log(`${i+1}.${mount[i]}`);
+  i++;
+}
