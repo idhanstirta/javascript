3bc95e8264908cae038ef35c37240516f53ec982
Hans Tirta <hanstirta235@gmail.com>
2025-11-03 10:41:14 +0700

    Add example of arrow functions in JavaScript

---
+++ arrow-function.js
@@ -0,0 +1,12 @@
+// arrow function
+// let ask = (question, yes, no) => confirm(question) ? yes() : no();
+// ask(
+// 'Do you agree?',
+// () => console.log('You agreed'),
+// () => console.log('You interrupted execution'),
+// );
+
+// arrow function the basic
+// let sum = (a,b) => a+b;
+// alert(sum(1,2));
+
